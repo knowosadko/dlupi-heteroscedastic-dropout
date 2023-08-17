9d5119229fbad369a6edd6c0c48808791572ed6c
Konrad Nowosadko <konrad@mida1.it.uu.se>
2023-08-17 17:20:16 +0200

    Remove unused folders.

--- cnns/data_utils/synset_utils.py
+++
@@ -1,8 +0,0 @@
-import cPickle as pkl
-
-def get_idx2synset_dict():
-    """ read python dict back from the file """
-    f = open('imagenet_idx2class.pkl', 'rb')
-    dict = pkl.load(f)
-    f.close()
-    return dict
